b9b3f1b026ba072a39c0a7ef997ea4d423f43b9e
MaherKouki <koukimaher386@gmail.com>
2025-07-18 02:04:37 +0100

    Stop entity

--- src/main/java/com/example/Transport/Entities/Stop.java
+++ src/main/java/com/example/Transport/Entities/Stop.java
@@ -1,10 +1,16 @@
 package com.example.Transport.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+
+
+@Entity
 
 public class Stop {
 
 
+    @Id
     private int id;
 
     private String stopName;
